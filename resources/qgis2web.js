577798bd7748e3edc42d0b31fb875f7545a79663
Dr. Arcady <prakharsrivastava6@gmail.com>
2026-09-09 20:55:21 +0530

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -714,17 +714,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// PBDB Pliocene Fossil Occurrences Layer
 	var pbdbVectorSource = new ol.source.Vector({
-	    loader: async function(extent, resolution, projection) {
-	        var response = await fetch('./layers/pbdb_pliocene.fgb');
-	        var container = this;
-	        for await (let feature of flatgeobuf.deserialize(response.body)) {
-	            var olFeature = new ol.format.GeoJSON().readFeature(feature, {
-	                dataProjection: 'EPSG:4326',
-	                featureProjection: 'EPSG:4326'
-	            });
-	            container.addFeature(olFeature);
-	        }
-	    }
+	    url: './layers/pbdb_pliocene.geojson',
+	    format: new ol.format.GeoJSON()
 	});
 	
 	var pbdbVectorLayer = new ol.layer.Vector({
