9376d1f06a781d9f40d7122670044dfe1730a09d
Dr. Arcady <prakharsrivastava6@gmail.com>
2026-09-10 05:21:33 +0530

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -1106,7 +1106,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 	// Reliable PBDB Fetch (Attaches all attributes safely)
-	showSpinner('Fetching Pliocene Fossils...');
+	showSpinner('Fetching Fossils');
 	fetch('https://paleobiodb.org/data1.2/occs/list.json?interval=Pliocene&show=coords,ident,attr')
 	    .then(function(res) { return res.json(); })
 	    .then(function(data) {
